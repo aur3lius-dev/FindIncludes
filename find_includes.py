d9a1b026c132a9ceef73e41e83ab90d813089bfa
aur3lius-dev <aur3lius-dev@users.noreply.github.com>
2016-08-22 21:10:11 -0400

    Added print locations option

--- find_includes.py
+++ find_includes.py
@@ -1,64 +1,94 @@
 from os import walk, path
+from sys import argv, exit
+import re
 import argparse
 
 
 class FindIncludes:
+	def __init__(self, i_dir, ignore_sys, find_missing):
+		# parse dir and files for include statements. Keep map of dir for look up
+		# if lookup fails add to naughty list
+		self.naughty_list = set()
+		self.dir_map = {}
+		self.inc_list = []
+		self.header_files = set()
+		self.ignore_sys = ignore_sys
+		self.i_dir = i_dir
+		self.find_missing = find_missing
+	
+	def main(self):
+		self.parse_dir()
+		if self.find_missing:
+			self.find_includes()
+		
+		
+	def parse_dir(self):
+		allowed_exts = ['.h','.hpp','.c','.cpp']
+		h_ext = ['.h', '.hpp']
+		for d_path, _, f_names in walk(self.i_dir):
+			for f in f_names:
+				ext = path.splitext(f)[1]
+				if ext in allowed_exts:
+					self.parse_file(d_path+"\\" + f)
+					if ext in h_ext:
+						self.header_files.add(f)
+			self.dir_map[d_path] = f_names
+				
+				
+	def parse_file(self, in_file):
+		# returns list of included files
+		sys_headers = ["<", "$"]
+		line_num = 0
+		with open(in_file) as f:
+			lines = f.readlines()
+		for line in lines:
+			line_num += 1
+			if line.startswith("#include "):
+				inc_name = line.split("#include ")[1].strip().strip('"')
+				if inc_name not in self.inc_list:
+					found_inc = Includes(inc_name)
+					if self.ignore_sys and found_inc.name[:1] in sys_headers:
+						pass
+					else:
+						self.inc_list.append(found_inc)
+				else:
+					found_inc = self.inc_list[self.inc_list.index(inc_name)]
+				found_inc.update_location(in_file, line_num) 
+				
+				
+	def find_includes(self):
+		missing_incs = list(self.inc_list)
+		for inc in missing_incs:
+			if inc.name in self.header_files:
+				self.inc_list.remove(inc)
 
-    def __init__(self, i_dir, ignore_sys, find_missing):
-        # parse dir and files for include statements. Keep map of dir for look up
-        # if lookup fails add to naughty list
-        self.naughty_list = set()
-        self.dir_map = {}
-        self.inc_list = set()
-        self.header_files = set()
-        self.ignore_sys = ignore_sys
-        self.i_dir = i_dir
-        self.find_missing = find_missing
-
-    def main(self):
-        self.parse_dir()
-        if self.find_missing:
-            self.find_includes()
-
-    def parse_dir(self):
-        inc_list = set()
-        dir_map = {}  # {dirname,[files]}
-        allowed_exts = ['.h', '.hpp', '.c', '.cpp']
-        h_ext = ['.h', '.hpp']
-        for d_path, _, f_names in walk(self.i_dir):
-            for f in f_names:
-                ext = path.splitext(f)[1]
-                if ext in allowed_exts:
-                    with open(d_path + "\\" + f) as in_file:
-                        self.parse_file(in_file.readlines())
-                    if ext in h_ext:
-                        self.header_files.add(f)
-            self.dir_map[d_path] = f_names
-
-    def parse_file(self, lines):
-        # returns list of included files
-        sys_headers = ["<", "$"]
-        for line in lines:
-            if line.startswith("#include "):
-                found_inc = line.split("#include ")[1].strip().strip('"')
-                if self.ignore_sys and found_inc[:1] in sys_headers:
-                    pass
-                else:
-                    self.inc_list.add(found_inc)
-
-    def find_includes(self):
-        missing_incs = list(self.inc_list)
-        for inc in missing_incs:
-            if inc in self.header_files:
-                self.inc_list.remove(inc)
-
+class Includes:
+	def __init__(self, name):
+		self.name = name
+		self.locations = set()  # Path/File: line#
+	
+	def update_location(self, filename, line_num):
+		loc = (filename, line_num)
+		self.locations.add(loc)
+	
+	def __eq__(self, other):
+		if type(other) is str:
+			return self.name == other
+		return self.name == other.name
+	
+	def __repr__(self):
+		return "%s\t%s" % (self.name, self.locations)
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--ignore_sys", "-i", action="store_true")
-parser.add_argument("-d", "--directory")
-parser.add_argument("-m", "--missing_headers", action="store_true")
+parser.add_argument("-i", "--ignore_sys", action="store_true", help="Ignore system header files")
+parser.add_argument("-m", "--missing_headers", action="store_true", help="Print missing header files only")
+parser.add_argument('-p', '--print_locations', action='store_true', help="Print file locations and line numbers for references to include statements")
+parser.add_argument("-d", "--directory", required=True)
 args = parser.parse_args()
 fi = FindIncludes(args.directory, args.ignore_sys, args.missing_headers)
 fi.main()
 for inc in fi.inc_list:
-    print(inc)
+	print (inc.name)
+	if args.print_locations:
+		for item in inc.locations:
+			print("\t%s:%s" % (item[0], item[1]))
